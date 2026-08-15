df586585145272632658d009b9b11b0fdf4f56b4
Rukshan Amodya <nmr.amodya@gmail.com>
2026-08-15 18:21:02 +0530

    fix: Synchronize JavaCompile and KotlinCompile tasks across all subprojects to Java 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,9 +20,13 @@ subprojects {
 }
 
 subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 }
